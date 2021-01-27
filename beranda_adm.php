244c86ee19991912271b70527d531a05e840a178
norzi21 <77970340+norzi21@users.noreply.github.com>
2021-01-27 11:46:34 +0700

    Add files via upload

---
+++ beranda_adm.php
@@ -0,0 +1,39 @@
+<div class="container">
+    <div class="row">
+        <div class ="col-xs-12">
+
+            <div class="alert alert-info">
+                Selamat Datang</strong>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <!--colomn kedua-->
+        <div class="col-sm-12 col-xs-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Halaman Administrator Sistem Informasi Penjualan Sepeda Motor PT. Anugrah Karya Abiwara</h3>
+                </div>
+                <div class="panel-body">
+                     <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                       <thead>
+                          <p align="center"><img src="img/download.png"></img></p>
+                          <p class="label-title" align="center"><strong>PT. Anugrah Karya Abiwara</strong></p>
+                          <p class="col-sm-12 col-xs-12" align="center">
+                            Selamat datang di halaman administrator Sistem Informasi Penjualan Sepeda Motor PT. Anugrah Karya Abiwara<br>
+                            Melalui halaman ini dapat dilakukan pengelolaan data penjualan, <br>
+                            dan juga pengelolaan laporan penjualan sepeda motor.<br><br>
+                            Akses menu Master Data pada bagian atas sistem untuk pengelolaan data penjualan dan data salesman.<br>
+                            Untuk mengelola laporan, dapat dilakukan dengan mengakses menu report pada bagian atas sistem.<br>
+                            Akses menu User untuk mengelola informasi tentang user yang login.<br><br><br>
+
+                          </p>
+
+                         </thead
+
+
+                    </table>
+                </div>
+            </div>
+        </div>
+</div>
---
+++ beranda_sales.php
@@ -0,0 +1,32 @@
+<div class="container">
+    <div class="row">
+        <div class ="col-xs-12">
+
+            <div class="alert alert-info">
+                Selamat Datang</strong>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <!--colomn kedua-->
+        <div class="col-sm-12 col-xs-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title"></h3>
+                </div>
+                <div class="panel-body">
+                     <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                       <thead>
+                          <<p align="center"><img src="img/download.png"></img></p>
+                          <p class="label-title" align="center"><strong>PT. Anugrah Karya Abiwara</strong></p>
+                          <p class="col-sm-12 col-xs-12" align="center">
+                            Selamat Datang di Sistem Informasi Penjualan Sepeda Motor PT. Anugrah Karya Abiwara<br>
+                          <p class="col-sm-12 col-xs-12" align="center">
+                           <div class="container">
+   
+
+                    </table>
+                </div>
+            </div>
+        </div>
+</div>
